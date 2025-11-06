cf2017a4b92196b9b0812091cdf11af3f273af20
Rafael-Saldanha <rafael.saldanha@aluno.senai.br>
2025-11-06 08:59:09 -0300

    commit do banco de dados

---
+++ src/main/java/br/com/senai/trabalho/configs/Swagger.java
@@ -0,0 +1,18 @@
+package br.com.senai.trabalho.configs;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Pizzaria",
+        version = "1.0",
+        description = "1"
+    )
+)
+public class Swagger {
+
+}
